22323988348e621bc9678e98eaaf989101d5c17d
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-02-24 21:56:57 -0300

    Obtendo usuários.

--- src/teste/basico/ObterUsuario.java
+++ src/teste/basico/ObterUsuario.java
@@ -6,23 +6,19 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class NovoUsuario {
+public class ObterUsuario {
 
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
         EntityManager em = emf.createEntityManager();
 
-        Usuario novoUsuario = new Usuario("Leonardo", "leo@gmail.com");
+        Usuario usuario = em.find(Usuario.class, 7L);
+        System.out.println(usuario.getNome());
 
-        em.getTransaction().begin();
-        em.persist(novoUsuario);
-        em.getTransaction().commit();
-        
-        System.out.println("O Id gerado foi: " + novoUsuario.getId());
-        
         em.clear();
         emf.close();
+
     }
 
 }
