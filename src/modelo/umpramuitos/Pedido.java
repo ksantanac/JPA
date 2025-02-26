07eec85997ba239b151496de468e62d8905f16ba
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-02-26 13:45:36 -0300

    Relacao 1-N 02.

--- src/modelo/umpramuitos/Pedido.java
+++ src/modelo/umpramuitos/Pedido.java
@@ -2,6 +2,7 @@ package modelo.umpramuitos;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Pedido {
@@ -12,6 +13,9 @@ public class Pedido {
 
     @Column(nullable = false)
     private Date data;
+
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido> itens;
 
     public Pedido() {
         this(new Date());
@@ -35,5 +39,13 @@ public class Pedido {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public List<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemPedido> itens) {
+        this.itens = itens;
     }
 }
