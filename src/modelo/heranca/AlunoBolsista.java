2dd5320d892def2b3c1bd4315e2f6568aeada392
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-02-26 23:44:38 -0300

    Herança.

---
+++ src/modelo/heranca/AlunoBolsista.java
@@ -0,0 +1,21 @@
+package modelo.heranca;
+
+public class AlunoBolsista extends Aluno{
+
+    private double valorBolsa;
+
+    public AlunoBolsista(){}
+
+    public AlunoBolsista(Long matricula, String nome, double valorBolsa) {
+        super(matricula, nome);
+        this.valorBolsa = valorBolsa;
+    }
+
+    public double getValorBolsa() {
+        return valorBolsa;
+    }
+
+    public void setValorBolsa(double valorBolsa) {
+        this.valorBolsa = valorBolsa;
+    }
+}
